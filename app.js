e8a2555501ce618e54a1f1cea270f2f5a5c1e363
Tarun Kumar <tarun0000111@gmail.com>
2025-12-30 23:07:16 +0530

    add a new feature 'Update'

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const fs = require('node:fs');
 
 const args = process.argv.slice(2);
 const op = args[0];
-const desc = args[1];
+
 
 const tasks = JSON.parse(fs.readFileSync('data.json', "utf-8"));
 
+
+
+// OPERATIONS
 const Add_task =  () => {
+    const desc = args[1];
+
     new_task = {
         id : Date.now(),
         description : desc,
@@ -17,7 +22,34 @@ const Add_task =  () => {
 
     tasks.push(new_task);
     fs.writeFileSync('data.json', JSON.stringify(tasks, null, 2));
-    console.log("Task Added Successfully");
+    console.log( `Task Added Successfully (${new_task.id})`);
+};
+
+
+const Update_task = () => {
+    const id = Number(args[1]);
+    const desc = args[2];
+
+    const task = tasks.find(t => t.id === id);
+
+    task.description = desc;
+    task.updatedAt = new Date().toISOString();
+
+    fs.writeFileSync('data.json', JSON.stringify(tasks, null, 2));
+    console.log( "Task Updated Successfully ");
+
+};
+
+
+
+const Delete_task = () =>{
+    const id = args[1];
+
+};
+
+
+const list=()=> {
+    
 };
 
 switch(op){
@@ -30,12 +62,12 @@ switch(op){
     case "Delete":
         Delete_task();
         break;
+    case "list":
+        list();
+        break;
     default:
-        console.log("No file Existed");  
+        console.log("Invalid Operation");  
 }
 
 
 
-
-const Update_task = () => {};
-const Delete_task = () =>{};
